f02489a691ac510f8bb8b7790df40da283ad418a
Danyiel Colin <danyiel5978@gmail.com>
2025-01-06 02:59:21 -0600

    feature Markdown: v1.1 added styles

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
+import ContentPage from './pages/ContentPage';
 
-export const router = createBrowserRouter([{ path: '/', element: <App /> }])
+export const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/content', element: <ContentPage /> },
+]);
